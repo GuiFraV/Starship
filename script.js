7a0cc42ffbfe3a8bb921dd42d028f9e6988e2a94
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-15 17:58:12 +0200

    Add cube rotation controls and update camera position in script.js

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ mesh.position.set(1, -0.1, 0)
 cubeTweaks.add(mesh.position, "x", -3, 3, 0.01)
 cubeTweaks.add(mesh.position, "y", -3, 3, 0.01)
 cubeTweaks.add(mesh.position, "z", -3, 3, 0.01)
+cubeTweaks
+    .add(mesh.rotation, "z")
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name("Cube rotation Z")
+
+cubeTweaks
+    .add(mesh.rotation, "x")
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name("Cube rotation X")
+
+cubeTweaks
+    .add(mesh.rotation, "y")
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name("Cube rotation Y")
 
 const axesHelper = new THREE.AxesHelper(1)
 scene.add(axesHelper)
@@ -40,7 +60,8 @@ window.addEventListener('resize', () => {
 })
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
-camera.position.z = 10
+camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 gui
@@ -50,13 +71,29 @@ gui
     .step(0.01)
     .name("Distance Camera")
 
+gui
+    .add(camera.rotation, 'x')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name("Angle Camera")
+
 
     const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
+const clock = new THREE.Clock();
+
 function animate(){
+
+    const elapsedTime = clock.getElapsedTime()
+
+    camera.position.y = elapsedTime
+    camera.lookAt(mesh.position)
+
+
     window.requestAnimationFrame(animate)
     renderer.render(scene, camera)
 }
